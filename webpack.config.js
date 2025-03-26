da75f0cf0609a154dfb6c06adc8f3c21bd0564cc
Pedro Henrique Santos Guimarães <pedro.guimaraes02@hotmail.com>
2025-03-26 16:53:44 -0300

    Auto stash before checking out "HEAD"

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,12 @@ module.exports = {
       favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
